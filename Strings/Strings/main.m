0b549baa5e19cbcb093dba0e25eda075d64f64be
Kyla <kylag44@gmail.com>
2018-06-21 21:25:01 -0400

    first commit

---
+++ Strings/Strings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Strings
+//
+//  Created by Kyla  on 2018-06-21.
+//  Copyright © 2018 Kyla . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
